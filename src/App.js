6cf770fff2a9c982175448414026dc453bda766b
88Xardas <45560376+88Xardas@users.noreply.github.com>
2023-05-30 20:35:45 +0200

    2035

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ayy lmao 2
+          AAAAAAAAAAHHH
         </p>
         <a
           className="App-link"
